3d9c317b93d782369493457a20425a3a76ae4983
Jcamilorada <Jcamilorada@live.com>
2014-05-13 13:41:12 -0500

    Added FrameFragment

---
+++ LayoutApplication/src/com/example/fragments/FrameFragment.java
@@ -0,0 +1,21 @@
+package com.example.fragments;
+
+import com.example.layoutapplication.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FrameFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_frame,
+				container, false);
+		return rootView;
+	}
+
+}
